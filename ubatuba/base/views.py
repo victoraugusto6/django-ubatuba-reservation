f968419ed8837e75957d8ac9df8e337f8be3a18c
Victor Augusto <victor.augusto60@gmail.com>
2021-09-09 12:49:26 -0300

    Created Home Page

--- ubatuba/base/views.py
+++ ubatuba/base/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Olá Django')
+    return render(request, 'base/home.html')
