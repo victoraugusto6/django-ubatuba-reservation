141633cf977d1252b09672fe47cbaec0f8477f99
Victor Augusto <victor.augusto60@gmail.com>
2021-09-09 12:49:26 -0300

    Created Home Page

--- ubatuba/base/tests/test_home.py
+++ ubatuba/base/tests/test_home.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from django_assertions import assert_contains
+
 
 @pytest.fixture
 def resp(client, db):
@@ -10,3 +12,19 @@ def resp(client, db):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_contains_home_link(resp):
+    assert_contains(resp, reverse('base:home'))
+
+
+def test_contains_dropdown(resp):
+    assert_contains(resp, '<li class="nav-item dropdown">')
+
+
+def test_contains_button_photos(resp):
+    assert_contains(resp, 'type="button">Fotos')
+
+
+def test_contains_button_map(resp):
+    assert_contains(resp, 'type="button">Mapa')
--- ubatuba/urls.py
+++ ubatuba/urls.py
@@ -5,10 +5,10 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.base, name='base')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='base')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
