8654c63b33e68ce505b036684105b77644aecaf8
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-10-02 00:28:09 -0300

    refactor form erros and tests

--- ubatuba/hospede/models.py
+++ ubatuba/hospede/models.py
@@ -11,9 +11,9 @@ AVALIACAO = (
 
 class Hospede(models.Model):
     nome = models.CharField(max_length=120, verbose_name='Nome')
-    cpf = models.CharField(unique=True, max_length=11, verbose_name='CPF')
+    cpf = models.CharField(unique=True, max_length=14, verbose_name='CPF')
     data_nascimento = models.DateField(verbose_name='Data de nascimento')
-    telefone = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=15)
     endereco = models.CharField(max_length=120, verbose_name='Endereço')
     avaliacao = models.IntegerField(choices=AVALIACAO, blank=True, null=True, verbose_name='Avaliação')
     observacao = models.TextField(verbose_name='Observações', blank=True, null=True)
