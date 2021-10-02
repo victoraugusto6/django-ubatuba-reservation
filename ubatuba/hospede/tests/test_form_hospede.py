34dab72920ca2fabf40a64c11ab8eb2f88bbab4d
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-10-02 00:04:55 -0300

    refactor form erros and tests

--- ubatuba/hospede/tests/test_form_hospede.py
+++ ubatuba/hospede/tests/test_form_hospede.py
@@ -1,8 +1,6 @@
 import pytest
-from django.urls import reverse
 
 from ubatuba.hospede.forms import HospedeForm
-from ubatuba.hospede.models import Hospede
 
 
 @pytest.mark.parametrize(
@@ -19,69 +17,29 @@ def test_form_success(db, nome, cpf, data_nascimento, telefone, endereco, avalia
         'observacao': observacao
     })
     assert form.is_valid()
-    assert form.cleaned_data['telefone'] == '11123451234'
 
 
-@pytest.fixture
-def resp_create_hospede(client, db, admin_user):
-    client.force_login(admin_user)
-    resp_create = client.post(reverse('hospede:create_hospedes'), data={
+def test_form_cpf_validation(db):
+    form = HospedeForm(data={
         'nome': 'Victor',
-        'cpf': '654.514.780-31',
+        'cpf': '397.738.908-48',
         'data_nascimento': '1995-12-19',
-        'telefone': '(11) 12345-1234',
+        'telefone': '(12) 99207-1544',
         'endereco': 'Rua 1'
     })
-    return resp_create
+
+    assert form.is_valid()
+    assert form.cleaned_data['cpf'] == '39773890848'
 
 
-def test_hospede_exists_bd(resp_create_hospede):
-    assert Hospede.objects.exists()
+def test_form_telefone_validation(db):
+    form = HospedeForm(data={
+        'nome': 'Victor',
+        'cpf': '397.738.908-48',
+        'data_nascimento': '1995-12-19',
+        'telefone': '(12) 99207-1544',
+        'endereco': 'Rua 1'
+    })
 
-
-def test_hospede_normalize_fields(resp_create_hospede):
-    assert Hospede.objects.get(
-        cpf='65451478031',
-        telefone='11123451234'
-    )
-
-
-@pytest.fixture
-def hospede(db):
-    return Hospede.objects.create(
-        nome='Victor Augusto',
-        cpf='654.514.780-31',
-        data_nascimento='1995-12-19',
-        telefone='(11) 12345-1234',
-        endereco='Rua 1'
-    )
-
-
-@pytest.fixture
-def resp_update_hospede(client, db, admin_user, hospede):
-    client.force_login(admin_user)
-    resp_update = client.post(reverse('hospede:update_hospede', kwargs={'pk': hospede.pk}),
-                              data={
-                                  'nome': 'Victor Augusto',
-                                  'cpf': '654.514.780-31',
-                                  'data_nascimento': '1995-12-19',
-                                  'telefone': '(11) 12345-1234',
-                                  'endereco': 'Rua 2'
-                              })
-    return resp_update
-
-
-def test_update_hospede(resp_update_hospede):
-    assert Hospede.objects.get(nome='Victor Augusto')
-    assert Hospede.objects.get(endereco='Rua 2')
-
-
-@pytest.fixture
-def resp_delete_hospede(client, db, admin_user, hospede):
-    client.force_login(admin_user)
-    resp_delete = client.post(reverse('hospede:delete_hospede', kwargs={'pk': hospede.pk}))
-    return resp_delete
-
-
-def test_delete_hospede(resp_delete_hospede):
-    assert not Hospede.objects.exists()
+    assert form.is_valid()
+    assert form.cleaned_data['telefone'] == '12992071544'
