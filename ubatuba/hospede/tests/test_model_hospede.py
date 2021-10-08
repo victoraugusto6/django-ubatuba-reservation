db678f5619090e8fe8b20286dfee614f78d05fbb
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-10-08 15:28:03 -0300

    Created test for ProtectedError

--- ubatuba/hospede/tests/test_model_hospede.py
+++ ubatuba/hospede/tests/test_model_hospede.py
@@ -1,9 +1,11 @@
 import pytest
+from django.db.models import ProtectedError
 from django.urls import reverse
 from model_bakery import baker
 
 from django_assertions import assert_contains
 from ubatuba.hospede.models import Hospede
+from ubatuba.reserva.models import Reserva
 
 
 @pytest.fixture
@@ -36,3 +38,25 @@ def test_hospede_in_page(resp, hospedes):
 
 def test_hospede_normalize_fields(db, hospede):
     assert Hospede.objects.exists()
+
+
+def test_delete_hospede_with_reserva_error(db):
+    hospede = Hospede.objects.create(
+        nome='Victor',
+        cpf='654.514.780-31',
+        telefone='(11) 12345-1234',
+    )
+    Reserva.objects.create(
+        hospede=hospede,
+        data_entrada='2021-09-28 12:40:00',
+        data_saida='2021-09-28 12:41:00',
+        qtd_pessoas_adulto=3,
+        qtd_pessoas_crianca=3,
+        pago=True,
+        valor_pago_total=100,
+        valor_pago_parcial=0,
+        pagara_limpeza=True,
+        observacao=''
+    )
+    Reserva.objects.get(data_entrada='2021-09-28 12:40:00').delete()
+    assert pytest.raises(ProtectedError)
